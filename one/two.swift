ce35ad79fecbb47155a07da1a364229169ded343
thajacob <tha.jacob2@gmail.com>
2017-04-06 20:57:41 +0100

    first

---
+++ one/two.swift
@@ -0,0 +1,39 @@
+//
+//  two.swift
+//  one
+//
+//  Created by jakub skrzypczynski on 16/12/2016.
+//  Copyright © 2016 test project. All rights reserved.
+//
+
+import UIKit
+
+class two: UIViewController {
+
+    @IBAction func back(_ segue: UIStoryboardSegue) {
+        
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
